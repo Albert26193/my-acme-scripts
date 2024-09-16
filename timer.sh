3fa37f6be0c95e9eb69ba196997b83120ff71046
Albert's server <albert990624@gmail.com>
2024-09-17 02:15:20 +0800

    update

--- timer.sh
+++ timer.sh
@@ -5,7 +5,7 @@
 function get_list {
 	local current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-	local -a subname_list="$(ls -al ${current_path} | awk '{print $NF}' | grep ".*cool_ecc$" | cut -d"." -f 1 | grep -v "albert")"
+	local -a subname_list="$(ls -al "${current_path}/.." | awk '{print $NF}' | grep ".*cool_ecc$" | cut -d"." -f 1 | grep -v "albert")"
 	if [[ $? -ne 0 ]]; then
 		return 1
 	fi
@@ -16,15 +16,26 @@ function get_list {
 
 function put_into_acme_auto {
 	local current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	local log_path="${current_path}/my_acme.log"
+
+	printf "\n%s\n" "----------------------------" >>"${log_path}"
+	printf "\n%s\n" "BEGIN TIME: $(date)" >>"${log_path}"
+	printf "\n%s\n" "----------------------------" >>"${log_path}"
+
 	local -a subname_list_arr
 	mapfile -t subname_list_arr <<<"$(get_list)"
 
 	for ele in "${subname_list_arr[@]}"; do
-		bash "${current_path}/auto_acme.sh" "${ele}" &
+		{
+			printf "%s\n" "CURRENT MODULE:$ele"
+			bash "${current_path}/auto_acme.sh" "${ele}"
+		} | flock "${log_path}" tee -a "${log_path}" &
 	done
-    
-    jobs
-    wait
+
+	wait
+	printf "\n%s\n" "----------------------------" >>"${log_path}"
+	printf "\n%s\n" "END TIME: $(date)" >>"${log_path}"
+	printf "\n%s\n" "----------------------------" >>"${log_path}"
 	return 0
 }
 
